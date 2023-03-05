eb9ef81978a3e69b0f62a85455a197a9bf83f0ba
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-03-05 17:56:22 +0000

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,17 +1,15 @@
-package com.company;
-
 import java.util.ArrayList;
 
 public class LinkedList<T> {
-    private Element<T> front;
-    private int length = 0;
+    private Element<T> Front;
+    private int Length = 0;
 
     public static void main(String[] args) {
     }
 
     public String asArray() {
         ArrayList<String> a = new ArrayList<>();
-        Element<T> e = front;
+        Element<T> e = Front;
 
         while (e != null) {
             a.add(e.getNode().getNode().getNode().toString());
@@ -30,12 +28,12 @@ public class LinkedList<T> {
         Element<T> Tail;
         Element<T> Insert;
 
-        if (front != null) {
-            Current = front;
+        if (Front != null) {
+            Current = Front;
 
             while (Current.Next() != null) {
-                if(Current.getNode().getNode().getNode() == value.getNode().getNode()){
-                    if (Current.getNode().getWeight() >= value.getWeight()) {
+                if (Current.getNode().getNode().getNode() == value.getNode().getNode()) {
+                    if (Current.getNode().getWeight() > value.getWeight()) {
                         Insert = new Element<T>(value, null, null);
                         Insert.Next(Current.Next());
                         Insert.Previous(Current.Previous());
@@ -46,57 +44,95 @@ public class LinkedList<T> {
                     }
                     return;
                 }
-                if(Current.getNode().getWeight() < value.getWeight()){
+                if (Current.getNode().getWeight() < value.getWeight()) {
                     Current = Current.Next();
-                } else{
-                    Insert = new Element<T>(value,null, null);
-                    Insert.Next(Current);
-                    Insert.Previous(Current.Previous());
-                    Current.Previous(Insert);
-                    Current.Previous().Next(Insert);
-                    length++;
+                } else {
+                    if (Current == Front) {
+                        Insert = new Element<T>(value, null, null);
+                        Insert.Next(Current);
+                        Insert.Previous(Current.Previous());
+                        Current.Previous(Insert);
+                        Front = Insert;
+                        Length++;
+                        return;
+                    } else {
+                        Insert = new Element<T>(value, null, null);
+                        Insert.Next(Current);
+                        Insert.Previous(Current.Previous());
+                        Current.Previous(Insert);
+                        Current.Previous().Next(Insert);
+                        Length++;
+                        return;
+                    }
+                }
+
+            }
+            if (Current.getNode().getNode().getNode() == value.getNode().getNode()) {
+                if (Current.getNode().getWeight() > value.getWeight()) {
+                    if (Current == Front) {
+                        Insert = new Element<T>(value, null, null);
+                        Insert.Next(Current);
+                        Insert.Previous(Current.Previous());
+                        Current.Previous(Insert);
+                    } else {
+                        Insert = new Element<T>(value, null, null);
+                        Insert.Previous(Current.Previous());
+                        Current.Previous().Next(Insert);
+                        Current.Previous(null);
+                    }
                     return;
                 }
-
             }
-            if(Current.getNode().getNode().getNode() == value.getNode().getNode()) {
-                if (Current.getNode().getWeight() >= value.getWeight()) {
-                    Insert = new Element<T>(value, null, null);
-                    Insert.Previous(Current.Previous());
-                    Current.Previous().Next(Insert);
-                    Current.Previous(null);
-                }
-                return;
-            }
-
-            Tail = new Element(value,Current, null);
+            Tail = new Element(value, Current, null);
             Current.Next(Tail);
-            length++;
-        } else {
-            front = new Element(value, null,null);
-            length++;
+            Length++;
+        }else {
+            Front = new Element(value, null, null);
+            Length++;
+            }
         }
-    }
 
-    public Element<T> pop() {
-        Element<T> popped;
-        if (!isEmpty() && length > 1) {
-            popped = front;
-            front.Next().Previous(null);
-            front = front.Next();
-            length--;
-            return popped;
-        }else if(length == 1) {
-            popped = front;
-            front = null;
-            length --;
-            return popped;
-        }else if (isEmpty()) {
-            throw new UnsupportedOperationException();
+
+        public Element<T> pop() {
+            Element<T> popped;
+            if (!isEmpty() && Length > 1) {
+                popped = Front;
+                Front.Next().Previous(null);
+                Front = Front.Next();
+                Length--;
+                return popped;
+            } else if (Length == 1) {
+                popped = Front;
+                Front = null;
+                Length--;
+                return popped;
+            } else if (isEmpty()) {
+                throw new UnsupportedOperationException("Linked List is empty");
+            }
+            return Front;
         }
-        return front;
-    }
-    public int length() {
-        return length;
-    }
+        public int length() {
+            return Length;
+        }
+
+        public QueueNode<T> search (QueueNode < T > value) {//Return true if the value exists in the Linked List
+            boolean found = false;
+            Element<T> Current = Front;
+            QueueNode<T> FoundValue;
+            if (!isEmpty()) {
+                while (Current.Next() != null) {
+                    if (Current.getNode().getNode().getNode() == value.getNode().getNode()) {
+                        FoundValue = Current.getNode();
+                        return FoundValue;
+                    } else {
+                        Current = Current.Next();
+                    }
+                }
+            }
+            if (!found) {
+                throw new IllegalArgumentException("Node with corresponding name not found");
+            } else {
+                return null;
+            }
+        }
 }
