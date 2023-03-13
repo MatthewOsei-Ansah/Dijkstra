48a5e0c9c8b578a4c8b63508e25e79d800942136
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-03-13 12:16:08 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,29 +10,44 @@ public class Main {
         characterGraph.addNode('E');
         characterGraph.addNode('F');
         characterGraph.addNode('G');
+        characterGraph.addNode('H');
+        characterGraph.addNode('I');
+        characterGraph.addNode('J');
 
         // Node A Connection Setup
         characterGraph.addConnection('A', 'B', 4);
-        characterGraph.addConnection('A','C', 2);
+        characterGraph.addConnection('A','C', 5);
+        characterGraph.addConnection('A','D', 8);
 
         // Node B Connection Setup
-        characterGraph.addConnection('B', 'C', 3);
-        characterGraph.addConnection('B','E',4);
-        characterGraph.addConnection('B','D',7);
+        characterGraph.addConnection('B','E',12);
+        characterGraph.addConnection('B','D',3);
 
         //Node C Setup
-        characterGraph.addConnection('C', 'D', 3);
+        characterGraph.addConnection('C', 'D', 1);
+        characterGraph.addConnection('C', 'F', 11);
 
         // Node D Setup
-        characterGraph.addConnection('D', 'E', 1);
-        characterGraph.addConnection('D', 'F',2);
+        characterGraph.addConnection('D', 'E', 9);
+        characterGraph.addConnection('D', 'F',4);
+        characterGraph.addConnection('D', 'G', 10);
 
         // Node E Setup
-        characterGraph.addConnection('E', 'G', 7);
-        characterGraph.addConnection('E', 'F', 5);
+        characterGraph.addConnection('E', 'G', 6);
+        characterGraph.addConnection('E', 'H', 10);
 
         // Node F Setup
         characterGraph.addConnection('F', 'G', 5);
+        characterGraph.addConnection('F', 'I', 11);
+
+        // Node G Setup
+        characterGraph.addConnection('G', 'J', 15);
+        characterGraph.addConnection('G', 'H', 3);
+        characterGraph.addConnection('G', 'I', 5);
+
+        // Node J setup
+        characterGraph.addConnection('J', 'H', 14);
+        characterGraph.addConnection('J', 'I', 8);
 
         // Other Stuff
         System.out.println(characterGraph.Display());
